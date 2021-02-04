f79d0d2029795d65cb773b3277c945fe6c4122cf
mdabdurrahmanabab <mdabdurrahmanabab@gmail.com>
2021-02-04 21:39:16 +0600

    null vs undefined programm solve

---
+++ null-vs-undefined.js
@@ -0,0 +1,15 @@
+let pakhi;
+
+function add(num1, num2){
+      console.log(num1 + num2);
+}
+const result = add(13, 80);
+
+const premik = {name:"smart songkar", phone:225131};
+console.log(premik.gf);
+
+let pun = undefined;
+console.log(pun);
+
+let age = [2, 5, 6, 14];
+console.log(age[11]);
