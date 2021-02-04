4b47bb4d9668f820380b86b3038d3b8820660fe1
mdabdurrahmanabab <mdabdurrahmanabab@gmail.com>
2021-02-04 21:59:49 +0600

    double epual and triple epual

---
+++ double-vs-triple.js
@@ -0,0 +1,13 @@
+const first = 1;
+const second = true;
+if (first == second){
+      console.log("condition is true");
+}else{
+      console.log("condition is false");
+}
+
+if (first === second){
+      console.log("condition is true");
+}else{
+      console.log("condition is false");
+}
