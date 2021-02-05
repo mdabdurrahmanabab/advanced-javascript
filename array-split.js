b2095f4eb321a1a115dc5536a95a64f0701d08db
mdabdurrahmanabab <mdabdurrahmanabab@gmail.com>
2021-02-05 11:21:19 +0600

    This is array split programm

---
+++ array-split.js
@@ -0,0 +1,11 @@
+const num = [1,2,3,4,5,6,7,8];
+const part = num.slice(2, 5);
+console.log(part)
+
+const num = [1,2,3,4,5,6,7,8];
+const moved = num.splice(2, 5);
+console.log(moved)
+
+const num = [1,2,3,4,5,6,7,8];
+const together = num.join(" ami ");
+console.log(together)
